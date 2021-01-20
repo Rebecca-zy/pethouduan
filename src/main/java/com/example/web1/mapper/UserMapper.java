7dcd0945b42151b0534c51b02bee4fd5d0e91b0e
Rebecca-zy <273146618@qq.com>
2021-01-21 01:00:05 +0800

    test

--- src/main/java/com/example/web1/mapper/UserMapper.java
+++ src/main/java/com/example/web1/mapper/UserMapper.java
@@ -1,7 +1,6 @@
 package com.example.web1.mapper;
 
 import com.example.web1.pojo.User;
-import com.example.web1.bean.UserBean;
 import org.apache.ibatis.annotations.*;
 
 //mapper层就是dao层，对数据进行增删改查处理
@@ -13,11 +12,9 @@ public interface UserMapper {
     @Select("SELECT * FROM user_info WHERE UI_USER_NAME=#{userName}")
     User getUserInfoByName(@Param("userName") String userName);
 
-
     @Insert("INSERT INTO user_info ( UI_USER_NAME, UI_PASSWORD, UI_STATUS,UI_CREATE_TIME, UI_ROLES )   VALUES ( #{UI_USER_NAME}, #{UI_PASSWORD},#{UI_STATUS},#{UI_CREATE_TIME},#{UI_ROLES}) ")
     @Options(useGeneratedKeys = true, keyProperty = "UI_ID")
     int addUser( User User);
 
-    UserBean getInfo(String name, String password);
 }
 
