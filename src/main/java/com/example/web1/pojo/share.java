18c955372f484a20c5be8a10c6aaaaf0976e70be
nnnnullll <1454672993@qq.com>
2021-01-26 16:40:40 +0800

    share

---
+++ src/main/java/com/example/web1/pojo/share.java
@@ -0,0 +1,77 @@
+package com.example.web1.pojo;
+
+public class share {
+    private int jlid;
+    private int yhid;
+    private int cwid;
+    private String yhm;
+    private String fbsj;
+    private String wz;
+    private String fqh;
+    private int sc;
+
+    public int getJlid() {
+        return jlid;
+    }
+
+    public void setJlid(int jlid) {
+        this.jlid = jlid;
+    }
+
+    public int getYhid() {
+        return yhid;
+    }
+
+    public void setYhid(int yhid) {
+        this.yhid = yhid;
+    }
+
+    public int getCwid() {
+        return cwid;
+    }
+
+    public void setCwid(int cwid) {
+        this.cwid = cwid;
+    }
+
+    public String getYhm() {
+        return yhm;
+    }
+
+    public void setYhm(String yhm) {
+        this.yhm = yhm;
+    }
+
+    public String getFbsj() {
+        return fbsj;
+    }
+
+    public void setFbsj(String fbsj) {
+        this.fbsj = fbsj;
+    }
+
+    public String getWz() {
+        return wz;
+    }
+
+    public void setWz(String wz) {
+        this.wz = wz;
+    }
+
+    public String getFqh() {
+        return fqh;
+    }
+
+    public void setFqh(String fqh) {
+        this.fqh = fqh;
+    }
+
+    public int getSc() {
+        return sc;
+    }
+
+    public void setSc(int sc) {
+        this.sc = sc;
+    }
+    
+}
