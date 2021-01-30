75fe9b3bd4fce606bdf5a689152873f0ae09e873
Rebecca-zy <273146618@qq.com>
2021-01-30 19:19:39 +0800

    模糊查询记录信息（用户名、最近一条文字动态及图片）

--- src/main/java/com/example/web1/controller/ShareController.java
+++ src/main/java/com/example/web1/controller/ShareController.java
@@ -1,10 +1,13 @@
 package com.example.web1.controller;
 
 import com.example.web1.pojo.Share;
+import com.example.web1.pojo.User;
 import com.example.web1.service.ShareService;
+import com.example.web1.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static java.lang.Integer.parseInt;
@@ -14,6 +17,8 @@ import static java.lang.Integer.parseInt;
 public class ShareController {
     @Autowired
     ShareService shareService;
+    @Autowired
+    UserInfoService userInfoService;
     //查数据库
     @PostMapping("/share")
     public Integer share(@RequestParam(value = "yhid") String yhid){
@@ -28,4 +33,14 @@ public class ShareController {
          return shareService.getShareByJlid(a);
      }
 
+     @PostMapping("/sharesearch")
+    public Map searchsearch(@RequestBody Map user) {
+         Map<String, List> result = new HashMap();
+         List<Share> shareinfo=shareService.getShareInfoBySimilarName(String.valueOf(user.get("wz")));
+         //List<User> userinfo=userInfoService.getUserInfoBySimilarName(String.valueOf(user.get("wz")));
+         result.put("shareinfo",shareinfo);
+         //result.put("userinfo",userinfo);
+
+         return result;
+     }
 }
--- src/main/java/com/example/web1/mapper/ShareMapper.java
+++ src/main/java/com/example/web1/mapper/ShareMapper.java
@@ -1,9 +1,12 @@
 package com.example.web1.mapper;
 
 import com.example.web1.pojo.Share;
+import com.example.web1.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface ShareMapper {
@@ -16,6 +19,6 @@ public interface ShareMapper {
     @Select("SELECT wz FROM share WHERE jlid=(SELECT max(jlid) FROM share WHERE yhid=#{yhid})")
     Share getLatestShareByYhid(@Param("yhid") Integer yhid);
 
-    //@Select("SELECT * FROM share WHERE jlid=(SELECT max(jlid) FROM share WHERE yhid=#{yhid})")
-    //Share getUserByYhid(@Param("yhid") Integer yhid);
+    @Select("SELECT * FROM share WHERE wz like CONCAT('%',#{wz},'%')")
+    List<Share> getShareInfoBySimilarName(@Param("wz") String wz);
 }
