468f64dbef0622546ad18763149bcc95ca853636
Rebecca-zy <273146618@qq.com>
2021-02-07 09:47:53 +0800

    2.7上传图片合并pdf、返回URL

---
+++ src/main/java/com/example/web1/PdfInfo/Img2PdfUtil.java
@@ -0,0 +1,81 @@
+package com.example.web1.PdfInfo;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.UUID;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 图片转pdf工具类
+ */
+public class Img2PdfUtil {
+
+    private static Logger logger = Logger.getLogger(Img2PdfUtil.class);
+    public static File MultipartFileToFile(MultipartFile multiFile) {
+        // 获取文件名
+        String fileName = multiFile.getOriginalFilename();
+        // 获取文件后缀
+        String prefix = fileName.substring(fileName.lastIndexOf("."));
+        // 若需要防止生成的临时文件重复,可以在文件名后添加随机码
+
+        try {
+            File file = File.createTempFile(fileName, prefix);
+            multiFile.transferTo(file);
+            return file;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     *
+     * @param outPdfFilepath 生成pdf文件路径
+     * @param imageFiles 需要转换的图片File类Array,按array的顺序合成图片
+     */
+    public static void imagesToPdf(String outPdfFilepath, File[] imageFiles) throws Exception {
+        logger.info("进入图片合成PDF工具方法");
+        File file = new File(outPdfFilepath);
+        //创建一个document对象。
+        Document document = new Document();
+        document.setMargins(0, 0, 0, 0);
+        //创建一个PdfWriter实例，
+        PdfWriter.getInstance(document, new FileOutputStream(file));
+        //打开文档。
+        document.open();
+        int len = imageFiles.length;
+        //在文档中增加图片。
+        for (int i = 0; i < len; i++) {
+            if (imageFiles[i].getName().toLowerCase().endsWith(".bmp")
+                    || imageFiles[i].getName().toLowerCase().endsWith(".jpg")
+                    || imageFiles[i].getName().toLowerCase().endsWith(".jpeg")
+                    || imageFiles[i].getName().toLowerCase().endsWith(".gif")
+                    || imageFiles[i].getName().toLowerCase().endsWith(".png")) {
+                String temp = imageFiles[i].getAbsolutePath();
+                logger.info("图片路径："+temp);
+                Image img = Image.getInstance(temp);
+                img.setAlignment(Image.ALIGN_CENTER);
+                //img.scaleAbsolute(597, 844);// 直接设定显示尺寸
+                // 根据图片大小设置页面，一定要先设置页面，再newPage（），否则无效
+                document.setPageSize(new Rectangle(img.getWidth(), img.getHeight()));
+                //document.setPageSize(new Rectangle(597, 844));
+                document.newPage();
+                document.add(img);
+            }
+        }
+        //关闭文档。
+        document.close();
+        logger.info("图片合成PDF完成");
+    }
+
+
+}
+
---
+++ src/main/java/com/example/web1/PhotoInfo/UploadController.java
@@ -0,0 +1,57 @@
+package com.example.web1.PhotoInfo;
+
+
+import com.example.web1.PdfInfo.Img2PdfUtil;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import static com.example.web1.PdfInfo.Img2PdfUtil.MultipartFileToFile;
+import static com.example.web1.PdfInfo.Img2PdfUtil.imagesToPdf;
+import static com.example.web1.PhotoInfo.FileUtils.getAccuracy;
+
+
+@CrossOrigin(origins = "*")
+@RestController
+public class UploadController {
+    @Resource
+    HttpServletRequest request;
+    //处理文件上传
+    @RequestMapping(value="/uploadimg", method = RequestMethod.POST)
+    public @ResponseBody String[] uploadImg(@RequestParam("file") MultipartFile[] file) throws Exception {
+        String url[] = new String[file.length];
+        int len=0;
+        for (MultipartFile multipartFile:file){
+            String fileName = multipartFile.getOriginalFilename();
+            //设置文件上传路径
+            String filePath = request.getSession().getServletContext().getRealPath("imgupload/");
+            //request.getSession().getServletContext()是获取的servlet容器对象。getRealPath("/") 获取实际路径，项目发布时，在容器中的实际路径。
+            try {
+                 Boolean res= FileUtils.uploadFile(multipartFile.getBytes(), filePath, fileName);
+                 url[len]=FileUtils.uploadImg(multipartFile,fileName);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            len++;
+        }
+        File imageFiles[]=new File[len];
+        for (int i=0;i<len;i++){
+            imageFiles[i]=MultipartFileToFile(file[i]);
+        }
+//        生成pdf文件的路径
+        String outPdfPath = "/Users/zhangyun/Desktop/PdfTest.pdf";
+        Img2PdfUtil.imagesToPdf(outPdfPath, imageFiles);
+
+        return url;
+
+    }
+
+
+
+
+
+}
